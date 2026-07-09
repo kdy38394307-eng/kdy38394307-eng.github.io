b919293189ad24c6eb639ce7c308107e1ea7f4e2
권대영 <kdy3839@naver.com>
2026-07-09 16:07:16 +0900

    update1

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //base : 'https://kdy38394307-eng.github.io/site1/'
+  //github action 쓸려고 생략함.
+})
